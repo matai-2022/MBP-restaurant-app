283a66484f4d6f0e43876fd1cc5d4780af5e2203
Lucas <luke.norris22@gmail.com>
2022-06-03 11:18:40 +1200

    add server.get to server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const fsPromises = require('fs').promises
 
 const server = express()
+
+const routes = require('./routes')
+
+const path = require('path')
+
+const filename = path.join(__dirname, 'data.json')
 
 // Server configuration
 server.use(express.static('public'))
@@ -14,3 +21,15 @@ server.set('view engine', 'hbs')
 // Your routes/router(s) should go here
 
 module.exports = server
+
+server.get('/', (req, res) => {
+  fsPromises
+    .readFile(filename, 'utf-8')
+    .then((restaurantData) => {
+      const parsedRestaurantData = JSON.parse(restaurantData)
+      return res.render('home', parsedRestaurantData)
+    })
+    .catch((err) => {
+      console.error(err, 'No data found')
+    })
+})
